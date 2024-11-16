c5bfb8aea5cea60c2ef29ad283c7e7bdbb885c08
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-11-16 13:03:55 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """
-
+Defines the BaseModel class.
 """
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
     def __init__(self):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         self.id = str(uuid.uuid4())
 
         self.created_at = datetime.utcnow()
@@ -15,11 +23,16 @@ class BaseModel:
 
     def save(self):
         """
+        Update updated_at with the current datetime.
         """
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """
+        Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
@@ -30,6 +43,7 @@ class BaseModel:
 
     def __str__(self):
         """
+        Return the print/str representation of the BaseModel instance.
         """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,19 @@
+#!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
+"""
 import unittest
 from models.base_model import BaseModel
 
 
 
 class TestBaseModel(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
+    
     def test_init(self):
         my_model = BaseModel()
 
@@ -12,6 +22,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(my_model.updated_at)
 
     def test_save(self):
+        """Unittests for testing save method of the BaseModel class."""
+
         my_model = BaseModel()
 
         initial_updated_at = my_model.updated_at
@@ -21,6 +33,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(initial_updated_at, current_updated_at)
 
     def test_to_dict(self):
+        """Unittests for testing to_dict method of the BaseModel class."""
+
         my_model = BaseModel()
         my_model_dict = my_model.to_dict()
         self.assertIsInstance(my_model_dict, dict)
@@ -30,6 +44,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(my_model_dict['updated_at'], my_model.updated_at.isoformat())
 
     def test_str(self):
+        """Unittest for testing str method of the BaseModel class."""
+
         my_model = BaseModel()
 
         self.assertTrue(str(my_model).startswith('[BaseModel]'))
